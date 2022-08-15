94be76a95c11543268afd87f93f69d6fa0558da4
Michal <michalszc05@gmail.com>
2022-08-15 14:43:42 +0200

    Added genres routes

--- src/api/routes/genreRoutes.js
+++ src/api/routes/genreRoutes.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 const tmp = (req, res) => res.send('NOT IMPLEMENTED YET');
 
-/// BOOK ROUTES ///
+/// GENRE ROUTES ///
 
-// /books - startpoint of every routes
+// /genres - startpoint of every routes
 
-// GET request for list of all books
+// GET request for list of all genres
 router.get('/', tmp);
 
-// POST request for creating book
+// POST request for creating a genre
 router.post('/create', tmp);
 
-// GET request for one book
+// GET request for one genre
 router.get('/:id', tmp);
 
-// DELETE request to delete book
+// DELETE request to delete genre
 router.delete('/:id/delete', tmp);
 
-// PUT request to update book
+// PUT request to update genre
 router.put('/:id/update', tmp);
 
 module.exports = router;
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bookRoutes = require('./bookRoutes');
+const genreRoutes = require('./genreRoutes');
 
 const router = express.Router();
 
 /// ROUTES ///
 
 router.use('/books', bookRoutes);
+router.use('/genres', genreRoutes);
 
 module.exports = router;

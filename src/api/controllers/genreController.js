9d803ca921b116eef7a48b36de6783be69548af0
Michal <michalszc05@gmail.com>
2022-08-17 23:30:55 +0200

    Added controllers for rest of the routes

--- src/api/controllers/genreController.js
+++ src/api/controllers/genreController.js
@@ -1,20 +1,60 @@
 const Genre = require('../models/genreModel');
 
 // Display list of all Genre.
-exports.genreList = function (req, res, next) {
-  Genre.find()
-    .sort([['name', 'ascending']])
-    .exec(function (error, result) {
-      if (error) { return next(error); }
-      res.json({ title: 'Genre List', genres: result.map(({ name }) => name) });
-    });
+exports.genreList = async function (req, res, next) {
+  try {
+    const genres = await Genre.find().sort([['name', 'ascending']]);
+    res.json({ title: 'Genre List', genres: genres.map(({ name }) => name) });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Display details for a specific Genre.
-exports.genreDetail = function (req, res, next) {
-  Genre.findById(req.params.id)
-    .exec(function (error, { name }) {
-      if (error) { return next(error); }
-      res.json({ title: 'Genre', genre: name, listOfBooks: null }); // ADD LIST OF BOOKS WITH THIS GENRE
-    });
+exports.genreDetail = async function (req, res, next) {
+  try {
+    res.json({ title: 'Genre', genre: res.genre.name, listOfBooks: null }); // ADD LIST OF BOOKS WITH THIS GENRE
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Handle Genre create.
+exports.genreCreate = async function (req, res, next) {
+  const genre = new Genre({
+    name: req.body.name
+  });
+  try {
+    const one = await Genre.findOne({ name: req.body.name });
+    if (one) {
+      throw Error('Genre already exists');
+    }
+    const newGenre = await genre.save();
+    res.status(201).json(newGenre);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Handle Genre delete.
+exports.genreDelete = async function (req, res, next) {
+  try {
+    await res.genre.remove();
+    res.json({ message: 'Deleted genre' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Handle Genre update
+exports.genreUpdate = async function (req, res, next) {
+  if (req.body?.name) {
+    res.genre.name = req.body.name;
+  }
+  try {
+    const updatedGenre = await res.genre.save();
+    res.json(updatedGenre);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };

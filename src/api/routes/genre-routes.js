122d460ef91d98a6d59de1a1d3a0f005cb7b1cd8
Michal <michalszc05@gmail.com>
2022-08-29 21:00:28 +0200

    Changed routes for creating genres

--- src/api/routes/genre-routes.js
+++ src/api/routes/genre-routes.js
@@ -60,49 +60,7 @@ const validators = require('../validations/genre-validation');
 router.get('/', genreList);
 
 /**
- * @api {POST} /genres/create Create a genre
- * @apiDescription Request for creating a genre
- * @apiVersion 1.0.0
- * @apiName CreateGenre
- * @apiGroup Genres
- *
- * @apiHeader {String} Content-Type=application/json
- * @apiBody {String{3.100}} name=Fantasy Genre name
- *
- * @apiSuccess {String} name Name of created genre
- * @apiSuccess {String} _id Id of created genre
- * @apiSuccess {Number} __v versionKey
- *
- * @apiSuccessExample {json} Success response (example):
- *  HTTP/1.1 201 Created
- *  {
- *    "name": "Fantasy",
- *    "_id": "6303e5462019452162263dfa",
- *    "__v": 0
- *  }
- *
- * @apiError BadRequest The server cannot process the request due to validation error or genre existence
- * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
- * @apiErrorExample {json} Validation error response (example):
- *  HTTP/1.1 400 Bad Request
- *  {
- *    "code": 400,
- *    "message": "Validation Error: body: \"name\" is not allowed to be empty",
- *    "errors": "Bad Request"
- *  }
- * @apiErrorExample {json} Genre existence response (example):
- *  HTTP/1.1 400 Bad Request
- *  {
- *    "code": 400,
- *    "message": "Genre(s) with name(s) Fantasy already exist(s)"
- *  }
- * @apiErrorExample {json} Internal Server Error response (example):
- *  HTTP/1.1 500 Internal Server Error
- */
-router.post('/create', validate(validators.genreCreate), genreCreate);
-
-/**
- * @api {POST} /genres/multiple/create Create multiple genres
+ * @api {POST} /genres/multiple Create multiple genres
  * @apiDescription Request for creating multiple genres
  * @apiVersion 1.0.0
  * @apiName CreateManyGenres
@@ -146,7 +104,96 @@ router.post('/create', validate(validators.genreCreate), genreCreate);
  * @apiErrorExample {json} Internal Server Error response (example):
  *  HTTP/1.1 500 Internal Server Error
  */
-router.post('/multiple/create', validate(validators.genreCreateMany), genreCreateMany);
+ router.post('/multiple', validate(validators.genreCreateMany), genreCreateMany);
+
+/**
+ * @api {POST} /genres Create a genre
+ * @apiDescription Request for creating a genre
+ * @apiVersion 1.0.0
+ * @apiName CreateGenre
+ * @apiGroup Genres
+ *
+ * @apiHeader {String} Content-Type=application/json
+ * @apiBody {String{3.100}} name=Fantasy Genre name
+ *
+ * @apiSuccess {String} name Name of created genre
+ * @apiSuccess {String} _id Id of created genre
+ * @apiSuccess {Number} __v versionKey
+ *
+ * @apiSuccessExample {json} Success response (example):
+ *  HTTP/1.1 201 Created
+ *  {
+ *    "name": "Fantasy",
+ *    "_id": "6303e5462019452162263dfa",
+ *    "__v": 0
+ *  }
+ *
+ * @apiError BadRequest The server cannot process the request due to validation error or genre existence
+ * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
+ * @apiErrorExample {json} Validation error response (example):
+ *  HTTP/1.1 400 Bad Request
+ *  {
+ *    "code": 400,
+ *    "message": "Validation Error: body: \"name\" is not allowed to be empty",
+ *    "errors": "Bad Request"
+ *  }
+ * @apiErrorExample {json} Genre existence response (example):
+ *  HTTP/1.1 400 Bad Request
+ *  {
+ *    "code": 400,
+ *    "message": "Genre(s) with name(s) Fantasy already exist(s)"
+ *  }
+ * @apiErrorExample {json} Internal Server Error response (example):
+ *  HTTP/1.1 500 Internal Server Error
+ */
+router.post('/', validate(validators.genreCreate), genreCreate);
+
+/**
+ * @api {GET} /genres/:id Get genre
+ * @apiDescription Request for one specific genre
+ * @apiVersion 1.0.0
+ * @apiName GetGenre
+ * @apiGroup Genres
+ *
+ * @apiParam {String{24}} id Genre id
+ * @apiParamExample {json} Request-Example:
+ *  {
+ *    "id": "62fd5e7e3037984b1b5effb2"
+ *  }
+ *
+ * @apiSuccess {Object} genre Requested genre
+ * @apiSuccess {String[]} listOfBooks List of books with this genre
+ * @apiSuccessExample {json} Success response (example):
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "genre": {
+ *      "_id": "62fd5e7e3037984b1b5effb2",
+ *      "name": "Fantasy",
+ *      "__v": 0
+ *    },
+ *    "listOfBooks": null
+ *  }
+ *
+ * @apiError BadRequest The server cannot process the request due to validation error
+ * @apiError NotFound The server cannot process the request due to incorrect id
+ * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
+ * @apiErrorExample {json} Validation error response (example):
+ *  HTTP/1.1 400 Bad Request
+ *  {
+ *    "code": 400,
+ *    "message": "Validation Error: params: \"id\" with value \"62fd5e7e3037984b1b5effbg\" fails to match the required pattern: /^[a-fA-F0-9]{24}$/",
+ *    "errors": "Bad Request"
+ *  }
+ * @apiErrorExample {json} Incorrect id error response (example):
+ *  HTTP/1.1 404 Not Found
+ *  {
+ *    "code": 404,
+ *    "message": "Cannot find genre with id 63091e5e4ec3fbc5c720db4c"
+ *  }
+ * @apiErrorExample {json} Internal Server Error response (example):
+ *  HTTP/1.1 500 Internal Server Error
+ */
+router.get('/:id', validate(validators.genreDetail), getGenre, genreDetail);
 
 /**
  * @api {DELETE} /genres/multiple Delete multiple genres
@@ -187,7 +234,48 @@ router.post('/multiple/create', validate(validators.genreCreateMany), genreCreat
  * @apiErrorExample {json} Internal Server Error response (example):
  *  HTTP/1.1 500 Internal Server Error
  */
-router.delete('/multiple', validate(validators.genreDeleteMany), checkExistence, genreDeleteMany);
+ router.delete('/multiple', validate(validators.genreDeleteMany), checkExistence, genreDeleteMany);
+
+/**
+ * @api {DELETE} /genres/:id Delete genre
+ * @apiDescription Request to delete genre
+ * @apiVersion 1.0.0
+ * @apiName DeleteGenre
+ * @apiGroup Genres
+ *
+ * @apiParam {String{24}} id Genre id
+ * @apiParamExample {json} Request-Example:
+ *  {
+ *    "id": "62fd5e7e3037984b1b5effb2"
+ *  }
+ *
+ * @apiSuccess {String} message Deleted genre
+ * @apiSuccessExample {json} Success response (example):
+ *  HTTP/1.1 200 OK
+ *  {
+ *     "message": "Deleted genre"
+ *  }
+ *
+ * @apiError BadRequest The server cannot process the request due to validation error
+ * @apiError NotFound The server cannot process the request due to incorrect id
+ * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
+ * @apiErrorExample {json} Validation error response (example):
+ *  HTTP/1.1 400 Bad Request
+ *  {
+ *    "code": 400,
+ *    "message": "Validation Error: params: \"id\" with value \"62fd5e7e3037984b1b5effbg\" fails to match the required pattern: /^[a-fA-F0-9]{24}$/",
+ *    "errors": "Bad Request"
+ *  }
+ * @apiErrorExample {json} Incorrect id error response (example):
+ *  HTTP/1.1 404 Not Found
+ *  {
+ *    "code": 404,
+ *    "message": "Cannot find genre with id 63091e5e4ec3fbc5c720db4c"
+ *  }
+ * @apiErrorExample {json} Internal Server Error response (example):
+ *  HTTP/1.1 500 Internal Server Error
+ */
+router.delete('/:id', validate(validators.genreDelete), getGenre, genreDelete);
 
 /**
  * @api {PATCH} /genres/multiple Update multiple genres
@@ -237,101 +325,7 @@ router.delete('/multiple', validate(validators.genreDeleteMany), checkExistence,
  * @apiErrorExample {json} Internal Server Error response (example):
  *  HTTP/1.1 500 Internal Server Error
  */
-router.patch('/multiple', validate(validators.genreUpdateMany), checkExistence, genreUpdateMany);
-
-// Use middlewares
-router.route('/:id')
-  .get(validate(validators.genreDetail), getGenre)
-  .delete(validate(validators.genreDelete), getGenre)
-  .patch(validate(validators.genreUpdate), getGenre);
-
-/**
- * @api {GET} /genres/:id Get genre
- * @apiDescription Request for one specific genre
- * @apiVersion 1.0.0
- * @apiName GetGenre
- * @apiGroup Genres
- *
- * @apiParam {String{24}} id Genre id
- * @apiParamExample {json} Request-Example:
- *  {
- *    "id": "62fd5e7e3037984b1b5effb2"
- *  }
- *
- * @apiSuccess {Object} genre Requested genre
- * @apiSuccess {String[]} listOfBooks List of books with this genre
- * @apiSuccessExample {json} Success response (example):
- *  HTTP/1.1 200 OK
- *  {
- *    "genre": {
- *      "_id": "62fd5e7e3037984b1b5effb2",
- *      "name": "Fantasy",
- *      "__v": 0
- *    },
- *    "listOfBooks": null
- *  }
- *
- * @apiError BadRequest The server cannot process the request due to validation error
- * @apiError NotFound The server cannot process the request due to incorrect id
- * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
- * @apiErrorExample {json} Validation error response (example):
- *  HTTP/1.1 400 Bad Request
- *  {
- *    "code": 400,
- *    "message": "Validation Error: params: \"id\" with value \"62fd5e7e3037984b1b5effbg\" fails to match the required pattern: /^[a-fA-F0-9]{24}$/",
- *    "errors": "Bad Request"
- *  }
- * @apiErrorExample {json} Incorrect id error response (example):
- *  HTTP/1.1 404 Not Found
- *  {
- *    "code": 404,
- *    "message": "Cannot find genre with id 63091e5e4ec3fbc5c720db4c"
- *  }
- * @apiErrorExample {json} Internal Server Error response (example):
- *  HTTP/1.1 500 Internal Server Error
- */
-router.get('/:id', genreDetail);
-
-/**
- * @api {DELETE} /genres/:id Delete genre
- * @apiDescription Request to delete genre
- * @apiVersion 1.0.0
- * @apiName DeleteGenre
- * @apiGroup Genres
- *
- * @apiParam {String{24}} id Genre id
- * @apiParamExample {json} Request-Example:
- *  {
- *    "id": "62fd5e7e3037984b1b5effb2"
- *  }
- *
- * @apiSuccess {String} message Deleted genre
- * @apiSuccessExample {json} Success response (example):
- *  HTTP/1.1 200 OK
- *  {
- *     "message": "Deleted genre"
- *  }
- *
- * @apiError BadRequest The server cannot process the request due to validation error
- * @apiError NotFound The server cannot process the request due to incorrect id
- * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
- * @apiErrorExample {json} Validation error response (example):
- *  HTTP/1.1 400 Bad Request
- *  {
- *    "code": 400,
- *    "message": "Validation Error: params: \"id\" with value \"62fd5e7e3037984b1b5effbg\" fails to match the required pattern: /^[a-fA-F0-9]{24}$/",
- *    "errors": "Bad Request"
- *  }
- * @apiErrorExample {json} Incorrect id error response (example):
- *  HTTP/1.1 404 Not Found
- *  {
- *    "code": 404,
- *    "message": "Cannot find genre with id 63091e5e4ec3fbc5c720db4c"
- *  }
- * @apiErrorExample {json} Internal Server Error response (example):
- *  HTTP/1.1 500 Internal Server Error
- */
-router.delete('/:id', genreDelete);
+ router.patch('/multiple', validate(validators.genreUpdateMany), checkExistence, genreUpdateMany);
 
 /**
  * @api {PATCH} /genres/:id Update genre
@@ -380,6 +374,6 @@ router.delete('/:id', genreDelete);
  * @apiErrorExample {json} Internal Server Error response (example):
  *  HTTP/1.1 500 Internal Server Error
  */
-router.patch('/:id', genreUpdate);
+router.patch('/:id', validate(validators.genreUpdate), getGenre, genreUpdate);
 
 module.exports = router;

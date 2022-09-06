9a2816054fc6eff5abcc09cae449be15265a4106
Michal <michalszc05@gmail.com>
2022-09-06 18:07:39 +0200

    Added utility function to check if date is before another date

--- src/api/utils/date.js
+++ src/api/utils/date.js
@@ -10,3 +10,14 @@ exports.addDays = function (date, days = 1) {
   const d = new Date(date);
   return new Date(d.getTime() + 86400000 * days);
 };
+
+/**
+ * Check if a Date is before another Date
+ * @public
+ * @param {String} date1
+ * @param {String} date2
+ * @returns {boolean} Returns true if the first date is before the second
+ */
+exports.isBefore = function isBefore (date1, date2) {
+  return new Date(date1) < new Date(date2);
+};

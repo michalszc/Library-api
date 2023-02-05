b171764a9d69fa7532f599c3b67fad2081768948
Michal <michalszc05@gmail.com>
2023-02-05 23:10:42 +0100

    Minor

--- src/config/express.js
+++ src/config/express.js
@@ -21,10 +21,10 @@ app.use(morgan(logs));
 
 // limit requests
 app.use(rateLimit({
-	windowMs: 60 * 60 * 1000, // 60 minutes
-	max: 100, // Limit each IP to 100 requests per `window`
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100, // Limit each IP to 100 requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
 }));
 
 // parse body params and attache them to req.body
